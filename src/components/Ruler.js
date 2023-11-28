2392ced0bb0c7713260892ff670a2d43174e703f
NadyaCodes <nadyacorscadden@gmail.com>
2023-11-28 16:29:02 -0500

    add ruler details

--- src/components/Ruler.js
+++ src/components/Ruler.js
@@ -7,7 +7,19 @@ export default function Ruler() {
     return cms.map((cm) => (
       <div key={cm} className="cm-line">
         <div className="cm-number">{cm}</div>
+        <div>
+          <div className="cm-quarter-line left"></div>
+          <div className="cm-quarter-line left"></div>
+          <div className="cm-quarter-line left"></div>
+          <div className="cm-quarter-line left"></div>
+        </div>
         <div className="cm-mid-line"></div>
+        <div>
+          <div className="cm-quarter-line right"></div>
+          <div className="cm-quarter-line right"></div>
+          <div className="cm-quarter-line right"></div>
+          <div className="cm-quarter-line right"></div>
+        </div>
       </div>
     ));
   };
@@ -15,10 +27,22 @@ export default function Ruler() {
   const inchesArray = () => {
     const inches = Array.from({ length: 12 }, (_, index) => index + 1);
 
+    const inchQuarters = () => {
+      return (
+        <div className="inch-smalls-container">
+          <div className="inch-eighth-line"></div>
+          <div className="inch-quarter-line"></div>
+          <div className="inch-eighth-line"></div>
+        </div>
+      );
+    };
+
     return inches.map((inch) => (
       <div key={inch} className="inch-line">
         <div className="inch-number">{inch}</div>
+        <div className="left-quarters">{inchQuarters()}</div>
         <div className="inch-mid-line"></div>
+        <div className="right-quarters">{inchQuarters()}</div>
       </div>
     ));
   };
@@ -28,6 +52,9 @@ export default function Ruler() {
       <div className="ruler-container">
         <div className="ruler">
           <div className="cms-container">{cmArray()}</div>
+          <div className="hole-container">
+            <div className="hole"></div>
+          </div>
           <div className="inches-container">{inchesArray()}</div>
         </div>
       </div>
